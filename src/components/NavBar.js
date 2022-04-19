74cc461540773e928585a27d51bf4a44e0b21894
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-19 15:31:40 -0300

    feat: added NavBar and SearchBar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BiArrowToTop } from 'react-icons/bi';
+import { GiCardPick, GiCardRandom } from 'react-icons/gi';
+import { useNavigate } from 'react-router-dom';
+import alrightSoundEffect from '../assets/sound-effects/alright.mp3';
+import '../sass/components/NavBar.css';
+import SearchBar from './SearchBar';
 
 function NavBar() {
+  const ringSound = new Audio(alrightSoundEffect);
+
+  const hidden = 'SearchBar SearchBar-hidden';
+
+  const [hideOrDisplay, setHideOrDisplay] = useState(hidden);
+
+  const navigate = useNavigate();
+
+  const handleSearchButtonClick = () => {
+    if (hideOrDisplay === 'SearchBar') {
+      setHideOrDisplay(hidden);
+    }
+    if (hideOrDisplay === hidden) {
+      setHideOrDisplay('SearchBar');
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/deck">Deck</Link></li>
-        <li><Link to="/game">Game</Link></li>
-      </ul>
-    </nav>
+    <div className="NavBar-container">
+      <nav className="NavBar">
+        <div className="NavBar-nav">
+          <button
+            type="button"
+            onClick={ scrollToTop }
+            className="NavBar-button ToTop"
+          >
+            <div className="NavBar-button-icon">
+              {/* eslint-disable-next-line react/jsx-max-depth */}
+              <BiArrowToTop />
+            </div>
+            <span>Voltar</span>
+          </button>
+          <button
+            type="button"
+            className="NavBar-button Search"
+            onClick={ handleSearchButtonClick }
+          >
+            <div className="NavBar-button-icon">
+              {/* eslint-disable-next-line react/jsx-max-depth */}
+              <GiCardPick />
+            </div>
+            <span>Pesquisar</span>
+          </button>
+          <button
+            type="button"
+            className="NavBar-button Play"
+            onClick={ () => {
+              ringSound.play();
+              navigate('/game');
+            } }
+          >
+            <div className="NavBar-button-icon">
+              {/* eslint-disable-next-line react/jsx-max-depth */}
+              <GiCardRandom />
+            </div>
+            <span>Jogar</span>
+          </button>
+        </div>
+        <SearchBar
+          hideOrDisplay={ hideOrDisplay }
+        />
+      </nav>
+    </div>
   );
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { BiFilter } from 'react-icons/bi';
+import { GiEmerald } from 'react-icons/gi';
+import '../sass/components/SearchBar.css';
 
-function SearchBar() {
+function SearchBar({ hideOrDisplay }) {
   return (
     <section>
-      SearchBar
-      {/* the user should be able to search for cards in the deck page */}
+      <section className={ hideOrDisplay }>
+        <div className="SearchBar-name-container">
+          <div className="SearchBar-filter-icon">
+            <BiFilter />
+          </div>
+          <input
+            id="name-filter"
+            name="nameSearch"
+            className="SearchBar-name"
+            type="text"
+            data-testid="name-filter"
+            placeholder="Nome da carta"
+            onChange={ ({ target }) => onInputChange(target.value, 'name') }
+          />
+        </div>
+        <select
+          id="rarity-filter"
+          name="raritySearch"
+          className="SearchBar-rarity"
+          data-testid="rare-filter"
+          onChange={ ({ target }) => onInputChange(target.value, 'rarity') }
+          defaultValue="todas"
+        >
+          <option value="todas">todas</option>
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
+        </select>
+        <label className="SearchBar-trunfo-label" htmlFor="trunfo-filter">
+          <div className="SearchBar-trunfo-icon">
+            <GiEmerald />
+          </div>
+          <span>Super Trunfo</span>
+          <input
+            id="trunfo-filter"
+            name="trunfoSearch"
+            className="SearchBar-trunfo"
+            type="checkbox"
+            data-testid="trunfo-filter"
+            onChange={ ({ target }) => onInputChange(target.checked, 'trunfo') }
+          />
+        </label>
+      </section>
     </section>
   );
 }
+
+SearchBar.propTypes = {
+  onInputChange: PropTypes.func.isRequired,
+  hideOrDisplay: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
