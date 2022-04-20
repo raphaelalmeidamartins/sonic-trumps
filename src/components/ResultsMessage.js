4efaac3fecb23505cc8866b9e8f33a24a6f81507
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-20 03:34:51 -0300

    fixed ResultsMessage bug.

--- src/components/ResultsMessage.js
+++ src/components/ResultsMessage.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import useStartGame from '../hooks/useStartGame';
 import drawGif from '../assets/gifs/draw.gif';
 import gameOver from '../assets/gifs/game-over.gif';
 import victory from '../assets/gifs/victory.gif';
+import useStartGame from '../hooks/useStartGame';
 import '../sass/components/ResultsMessage.css';
 
 function ResultsMessage() {
   const wins = useSelector((state) => state.game.wins);
   const cpuWins = useSelector((state) => state.game.cpuWins);
 
-  // const startGame = useStartGame();
+  const startGame = useStartGame();
+
+  const button = <button type="button" onClick={ startGame }>Jogar de novo</button>;
 
   if (wins === cpuWins) {
     return (
       <section className="ResultsMessage">
         <img src={ drawGif } alt="draw" />
         <p>Empate!</p>
+        {button}
       </section>
     );
   }
@@ -26,12 +29,14 @@ function ResultsMessage() {
       <section className="ResultsMessage">
         <img src={ victory } alt="victory pose" />
         <p>Parabéns, você ganhou!</p>
+        {button}
       </section>
     )
     : (
       <section className="ResultsMessage">
         <img src={ gameOver } alt="game over" />
         <p>Que pena, você perdeu!</p>
+        {button}
       </section>
     );
 }
--- src/pages/TitlePage.js
+++ src/pages/TitlePage.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GiCardPick, GiCardRandom } from 'react-icons/gi';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import alrightSoundEffect from '../assets/sound-effects/alright.mp3';
 import ringSoundEffect from '../assets/sound-effects/ring.mp3';
+import useStartGame from '../hooks/useStartGame';
 import '../sass/pages/TitlePage.css';
 
 function TitlePage() {
   const navigate = useNavigate();
   const alrightSound = new Audio(alrightSoundEffect);
   const ringSound = new Audio(ringSoundEffect);
+
+  const startGame = useStartGame();
+
+  // used like componentDidMount, only runs when the component is mounted
+  useEffect(startGame, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="TitlePage">
