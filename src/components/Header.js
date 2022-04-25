9310c3f771623943d74355c480d52f7ec2df59c7
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-25 11:14:30 -0300

    feat: the logo is now a link to the title screen.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import '../sass/components/Header.css';
 
 function Header() {
   return (
     <header className="Header">
-      <img src={ logo } alt="Sonic Trunfo" />
+      <Link to="/">
+        <img src={ logo } alt="Sonic Trunfo" />
+      </Link>
     </header>
   );
 }
